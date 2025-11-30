f4d067553b1d3bc44412b828a7539e34e0ba1584
tahakhurram1 <126037622+tahakhurram1@users.noreply.github.com>
2025-11-29 20:39:16 -0600

    Add files via upload

--- src/com/checkmates/util/DatabaseConnection.java
+++ src/com/checkmates/util/DatabaseConnection.java
@@ -8,7 +8,7 @@ public class DatabaseConnection {
 
     private static final String URL = "jdbc:mysql://localhost:3306/checkmates";
     private static final String USER = "root";
-    private static final String PASSWORD = "your_password";
+    private static final String PASSWORD = ""; // MySQL installed without password - leave empty or set one with mysql_secure_installation
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
